1bfe923c9e4980d5202bebfad8326730a229f444
lixiuxiu777 <103087428+lixiuxiu777@users.noreply.github.com>
2023-01-29 10:43:05 +0800

    Add files via upload

---
+++ precache-manifest.f1b553640d86ca08721b6f1f689b4295.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a1206beb68969b22c9",
+    "url": "css/app.5fc57530.css"
+  },
+  {
+    "revision": "8d57f12e8cc04b462e3f",
+    "url": "css/chunk-vendors.094863c6.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "86b25854eb8a0e9da15e18674ffec68a",
+    "url": "index.html"
+  },
+  {
+    "revision": "f3a2a5ca3ce5f21ffedfde9b0a869a7f",
+    "url": "ixarea-stats.js"
+  },
+  {
+    "revision": "fd79347bd7c3bb4be90a76dd00a90857",
+    "url": "js/0.dde8253a.worker.js"
+  },
+  {
+    "revision": "04a1206beb68969b22c9",
+    "url": "js/app.d5ac0dac.js"
+  },
+  {
+    "revision": "8d57f12e8cc04b462e3f",
+    "url": "js/chunk-vendors.e75bd7b3.js"
+  },
+  {
+    "revision": "7eb24d0a24f0d87d81196b05ae7fbb14",
+    "url": "loader.js"
+  },
+  {
+    "revision": "523b1a2eae8cb533fa6bd73831308f09",
+    "url": "static/kgm.mask"
+  },
+  {
+    "revision": "cd1d395410107c66b4534ec93f0073d3",
+    "url": "web-manifest.json"
+  }
+]);
